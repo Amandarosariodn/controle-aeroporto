5e1ffcb6cb8ab2cae28af640036d9ed5653e4cc3
gabfiorio <fioriogabriel123@gmail.com>
2025-10-21 20:18:21 -0300

    Adiciona entidade checkin

--- Models/Checkin.java
+++ Models/Checkin.java
@@ -2,4 +2,27 @@ package Models;
 
 public class Checkin {
 
+  private String CPF;
+  private String Bagagem;
+  private String vooReservado;
+  
+  public String getCPF() {
+    return CPF;
+  }
+  public void setCPF(String cPF) {
+    CPF = cPF;
+  }
+  public String getBagagem() {
+    return Bagagem;
+  }
+  public void setBagagem(String bagagem) {
+    Bagagem = bagagem;
+  }
+  public String getVooReservado() {
+    return vooReservado;
+  }
+  public void setVooReservado(String vooReservado) {
+    this.vooReservado = vooReservado;
+
+  }
 }
